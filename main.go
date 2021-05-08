a062c203f57b07d4b70628313802eea84592b1ae
iNaz07 <ekrezan.94>
2021-05-09 00:01:59 +0600

    api created, wo css

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sort"
+)
+
+type Player struct {
+	Name  string `json:"Name"`
+	Score int    `json:"Score"`
+	Time  string `json:"Time"`
+}
+
+var PlayersBoard []Player
+var filename = "scoreboard.json"
+func readJsonFile() []byte {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return content
+}
+
+func writeDataToJson(data []byte) {
+	err := ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sortData() {
+	sort.SliceStable(PlayersBoard, func(i, j int) bool {
+		return PlayersBoard[i].Score > PlayersBoard[j].Score
+	})
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	var player Player
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &player)
+	PlayersBoard = append(PlayersBoard, player)
+}
+
+func createNewPlayer(w http.ResponseWriter, r *http.Request) {
+	
+	content := readJsonFile()
+	json.Unmarshal(content, &PlayersBoard)
+	if r.Method == "GET" {
+		sortData()
+	}
+	if r.Method == "POST" {
+		apiHandler(w, r)
+		sortData()
+		pB, _ := json.Marshal(PlayersBoard)
+		writeDataToJson(pB)
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Accept", "application/json")
+	json.NewEncoder(w).Encode(PlayersBoard)
+}
+
+func handleReguests() {
+	http.HandleFunc("/addplayer", createNewPlayer)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handleReguests()
+}
+ 
---
+++ score.js
@@ -0,0 +1,64 @@
+async function addPlayer() {
+    
+let player = {
+    Name: document.getElementById("pname"),
+    Score: score,
+    Time: timer.value.split(' ')[1]
+}
+    let url = 'http://localhost:8080/addplayer'
+    let init = {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        'Access-Control-Allow-Origin': '*',
+        body: JSON.stringify(player)
+    }
+    try {
+        let response = await fetch(url, init)
+            let res =  await response.json()
+            createTable(res)
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+async function allPlayer() {
+    let url = 'http://localhost:8080/addplayer'
+    let init = {
+        method: 'GET',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        'Access-Control-Allow-Origin': '*',
+    }
+    try {
+        let response = await fetch(url, init)
+        let res = await response.json()
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+function createTable(res) {
+    document.getElementById("player").style.display = "none"
+    document.getElementById("board").style.display = "block"
+    let table = document.getElementById("table")
+    for (let i = 0; i < 5; i++) {
+        let tr = document.createElement("div")
+        tr.classList.add("row", "result")
+        let td = document.createElement("div")
+        td.classList.add("column", "result")
+        td.innerHTML = i+1
+        tr.appendChild(td)
+        for (const val of Object.values(res[i])) {
+            let td = document.createElement("div")
+            td.classList.add("column", "result")
+            td.innerHTML = val
+            tr.appendChild(td) 
+        }
+        table.appendChild(tr)
+    }
+}
